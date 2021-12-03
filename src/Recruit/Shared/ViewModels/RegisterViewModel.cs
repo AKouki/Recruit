1d5befca4b3f89774f408dee692ad98a21c934df
Albion Kouki <koukialbion@yahoo.com>
2021-12-03 14:58:11 +0200

    Added ConfirmPassword to RegisterViewModel

--- src/Recruit/Shared/ViewModels/RegisterViewModel.cs
+++ src/Recruit/Shared/ViewModels/RegisterViewModel.cs
@@ -14,5 +14,10 @@ namespace Recruit.Shared.ViewModels
         [Required]
         [DataType(DataType.Password)]
         public string? Password { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        [Compare("Password", ErrorMessage = "The password and confirmation password do not match.")]
+        public string? ConfirmPassword { get; set; }
     }
 }
