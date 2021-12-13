17e1bed1e5fe20d89428366b82cd6b246e2921e9
Albion Kouki <koukialbion@yahoo.com>
2021-12-13 23:25:36 +0200

    Implemented outside click detector in order to hide ApplicantDetails overlay when user clicks outside of it

---
+++ src/Recruit/Client/wwwroot/js/scripts.js
@@ -0,0 +1,25 @@
+﻿var _instance = null;
+var _methodName = null;
+
+var HandleOutsideDivClick = function (event) {
+    var div = document.querySelector(".applicant-details.show");
+    if (div) {
+        if (event.target.closest(".applicant-details.show") ||
+            event.target.closest(".close-button") ||
+            event.target.closest("a")) {
+            return;
+        }
+
+        _instance.invokeMethodAsync(_methodName);
+    }
+};
+
+export function AddOutsideDivClickListener(instance, methodName) {
+    _instance = instance;
+    _methodName = methodName;
+    document.addEventListener("click", HandleOutsideDivClick);
+}
+
+export function RemoveOutsideDivClickListener() {
+    document.removeEventListener("click", HandleOutsideDivClick);
+}
