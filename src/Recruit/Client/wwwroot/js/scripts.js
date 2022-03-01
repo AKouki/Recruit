684745a60c3399f19c18ddc58e940e0ad88a2791
Albion Kouki <koukialbion@yahoo.com>
2022-03-01 12:14:11 +0200

    Fixed a bug with outside click detector not working for ApplicantDetails overlay in SearchForm

--- src/Recruit/Client/wwwroot/js/scripts.js
+++ src/Recruit/Client/wwwroot/js/scripts.js
@@ -1,6 +1,6 @@
 ﻿export function AddOutsideDivClickListener(instance, methodName) {
     $("body").click(function (event) {
-        var div = document.querySelector(".applicant-details.show");
+        var div = document.querySelectorAll(".applicant-details.show");
         if (div) {
             if (event.target.id != "applicant-details" && $(event.target).parents("#applicant-details").length == 0) {
                 instance.invokeMethodAsync(methodName);
