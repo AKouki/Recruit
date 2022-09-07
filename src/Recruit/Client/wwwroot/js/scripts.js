c8245de369648ae8a0beda625e60fd3d0b1713bc
Albion Kouki <koukialbion@yahoo.com>
2022-09-07 10:41:27 +0300

    Fixed a bug that was causing the modal to close

--- src/Recruit/Client/wwwroot/js/scripts.js
+++ src/Recruit/Client/wwwroot/js/scripts.js
@@ -1,7 +1,7 @@
 ﻿export function AddOutsideDivClickListener(instance, methodName) {
     $("body").click(function (event) {
         var div = document.querySelectorAll(".applicant-details.show");
-        if (div) {
+        if (div.length == 1) {
             if (event.target.id != "applicant-details" &&
                 $(event.target).parents("#applicant-details").length == 0 &&
                 $(event.target).parents(".modal").length == 0) {
